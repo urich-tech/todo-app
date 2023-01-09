21dda89d8233da103c960bf1511147259974dca6
urich-tech <cybtech127@gmail.com>
2023-01-09 03:27:19 +0100

    version commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.hosts << "todo-app-1-production-6619.up.railway.app"
+  
 end
